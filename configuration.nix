d11011f0aee9fd2d3a07cb4795618dfc72e2a72f
Shu Lin <linshu1729@protonmail.com>
2022-05-10 19:10:16 +0800

    modify localisation

--- configuration.nix
+++ configuration.nix
@@ -46,8 +46,8 @@
   
   # Configure keymap in X11
   services.xserver.layout = "us";
-  services.xserver.xkbVariant = "intl"; # included xkbOption "eurosign:5"
-  services.xserver.xkbOptions = "caps:none";  #/usr/share/X11/xkb/rules/evdev.lst
+  services.xserver.xkbVariant = "altgr-intl"; # included xkbOption "eurosign:5"
+  services.xserver.xkbOptions = "caps:none";  # /usr/share/X11/xkb/rules/evdev.lst
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
