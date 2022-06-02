a55429c69765609ca2b1537579308da9827f7c1e
sauricat <linshu1729@protonmail.com>
2022-06-02 21:16:05 +0800

    fish: split modules

--- home/fish.nix
+++ home/fish.nix
@@ -1,4 +1,10 @@
-{ ... }:
+{ lib, ... }:
+let
+  myinit = lib.strings.concatStringsSep "\n" [
+    "set fish_prompt_pwd_dir_length 0" # make prompt show full path
+    (builtins.readFile ./fish/guix_integration.fish)
+    (builtins.readFile ./fish/vterm_integration.fish) ];
+in
 {
   programs.fish = {
     enable = true;
@@ -12,7 +18,7 @@
       # Other commands
       c = "code .";
       f = "find /nix/store -name";
-      n = "nix"; nse = "nix search nixpkgs";
+      n = "nix"; nre = "nix repl '<nixpkgs>'"; nse = "nix search nixpkgs";
       
       # FIXME: abbrs cannot be sticked together
       s = "sudo";
@@ -25,80 +31,15 @@
       ptree = "procs --tree";
       hash = "nix-hash --flat --base32 --type sha256 $argv";
       nshp = "nix shell nixpkgs#$argv";
-      rm = "echo 'Directly `rm` is disabled, use `trash` (or alias `trm`) instead.'";
-      gcm = ''
-        git commit -m "$argv"
-      '';
+      rm = "echo 'Directly `rm` is disabled, use `trash` (or alias `t`) instead.'";
 
       # Prompt
       fish_greeting = "";
-      fish_prompt = ''
-        # Defined originally in /nix/store/vkf0h13wialm2c6i3ylbqnq540gjygvm-fish-3.4.1/share/fish/functions/fish_prompt.fish @ line 4
-        # function fish_prompt --description 'Write out the prompt'
-        set -l last_pipestatus $pipestatus
-        set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
-        set -l normal (set_color normal)
-        set -q fish_color_status
-        or set -g fish_color_status --background=red white
-    
-        # Color the prompt differently when we're root        set -l color_cwd $fish_color_cwd
-        set -l suffix '>'
-        if functions -q fish_is_root_user; and fish_is_root_user
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            end
-            set suffix '#'
-        end
-
-        # Write pipestatus
-        # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
-        set -l bold_flag --bold
-        set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
-        if test $__fish_prompt_status_generation = $status_generation
-            set bold_flag
-        end
-        set __fish_prompt_status_generation $status_generation
-        set -l status_color (set_color $fish_color_status)
-        set -l statusb_color (set_color $bold_flag $fish_color_status)
-        set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
-
-        echo 
-        echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " " $prompt_status "\n" $suffix " "
-      '';
-    
+      fish_prompt = lib.strings.removePrefix "function fish_prompt" 
+        (lib.strings.removeSuffix "end\n" # stubborn, need further improvement
+          (builtins.readFile ./fish/fish_prompt.fish));
     };
 
-    # guix integration
-    interactiveShellInit = ''
-      set -x PATH $PATH /var/guix/profiles/per-user/root/current-guix/bin/
-      set GUIX_LOCPATH $HOME/.guix-profile/lib/locale
-    '';
-    
-    # vterm integration
-    shellInit = ''
-      function vterm_printf;
-        if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
-          # tell tmux to pass the escape sequences through
-          printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
-        else if string match -q -- "screen*" "$TERM"
-          # GNU screen (screen, screen-256color, screen-256color-bce)
-          printf "\eP\e]%s\007\e\\" "$argv"
-        else
-          printf "\e]%s\e\\" "$argv"
-        end
-      end
-      function vterm_prompt_end;
-        vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)
-      end
-      functions --copy fish_prompt vterm_old_fish_prompt
-      function fish_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
-        # Remove the trailing newline from the original prompt. This is done
-        # using the string builtin from fish, but to make sure any escape codes
-        # are correctly interpreted, use %b for printf.
-        printf "%b" (string join "\n" (vterm_old_fish_prompt))
-        vterm_prompt_end
-      end
-    '';
-
+    shellInit = myinit; 
   };
 }
---
+++ home/fish/guix_integration.fish
@@ -0,0 +1,2 @@
+set -x PATH $PATH /var/guix/profiles/per-user/root/current-guix/bin/
+set GUIX_LOCPATH $HOME/.guix-profile/lib/locale
