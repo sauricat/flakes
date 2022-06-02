5b916a1044efc12731ccc06f1c7fc25e067c283f
sauricat <linshu1729@protonmail.com>
2022-06-02 16:28:05 +0800

    fish: fix emacs-vterm integration; improve prompt structure

--- home/fish.nix
+++ home/fish.nix
@@ -63,20 +63,7 @@
         set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
         echo 
-        echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
-      '';
-      
-      # vterm integration
-      vterm_printf = ''
-        if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end 
-            # tell tmux to pass the escape sequences through
-            printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
-        else if string match -q -- "screen*" "$TERM"
-            # GNU screen (screen, screen-256color, screen-256color-bce)
-            printf "\eP\e]%s\007\e\\" "$argv"
-        else
-            printf "\e]%s\e\\" "$argv"
-        end
+        echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " " $prompt_status "\n" $suffix " "
       '';
     
     };
@@ -86,7 +73,32 @@
       set -x PATH $PATH /var/guix/profiles/per-user/root/current-guix/bin/
       set GUIX_LOCPATH $HOME/.guix-profile/lib/locale
     '';
-
+    
+    # vterm integration
+    shellInit = ''
+      function vterm_printf;
+        if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
+          # tell tmux to pass the escape sequences through
+          printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+        else if string match -q -- "screen*" "$TERM"
+          # GNU screen (screen, screen-256color, screen-256color-bce)
+          printf "\eP\e]%s\007\e\\" "$argv"
+        else
+          printf "\e]%s\e\\" "$argv"
+        end
+      end
+      function vterm_prompt_end;
+        vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)
+      end
+      functions --copy fish_prompt vterm_old_fish_prompt
+      function fish_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
+        # Remove the trailing newline from the original prompt. This is done
+        # using the string builtin from fish, but to make sure any escape codes
+        # are correctly interpreted, use %b for printf.
+        printf "%b" (string join "\n" (vterm_old_fish_prompt))
+        vterm_prompt_end
+      end
+    '';
 
   };
 }
