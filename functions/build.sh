ef7ba34e4a93820855d71374f5ff6c8673e5b32a
sauricat <linshu1729@protonmail.com>
2022-06-03 01:06:31 +0800

    build.sh: update interaction
    1. Now we do not need to manually add “sudo” before some operations
    requiring root privilege -- the shell script now can urge us to check
    our privilege in some specific occasions.
    
    2. The interaction paragraphs are re-designed to make it clearer. For
    example, we use colors, but maybe too many colors.
    
    3. Some codes are refactored into an « oxalicaeic » style, for
    example, `if [ foo != "" ]` -> `if [[ -z foo ]]`. However, some are
    not: we don’t use `if <cond>; then`. Instead, we write `if` and `then`
    in two separate lines.

--- functions/build.sh
+++ functions/build.sh
@@ -2,74 +2,79 @@
 projectDir="$(dirname "$0")"/..
 cd $projectDir
 
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]
+if [[ "$1" == "-h" || "$1" == "--help" ]]
 then
-    echo "
+    exec echo "
 Usage:
-  $0 [Argument]
+  $0 [OPTION]
 
-Argument:
+Options:
   -h, --help: Open help page.
   -d, --default: Use default settings. 
 "
-    exit 1
 fi
 
 # ~~ Interaction ~~
-if [ "$1" != "-d" ] && [ "$1" != "--default" ]
+if [[ "$1" != "-d" && "$1" != "--default" ]]
 then
-    echo -e "Update 'flake.lock'? (default 'n' by [RET])"
-    read -p "> (y/n) ... " updateFlake
+    echo -e "\nUpdate '\033[1;36mflake\033[0m.lock'? (y/\033[1;4mn\033[0m)"
+    read -p ">> " updateFlake
 
-    echo -e "\nAre you \033[1;31mbuilding a LiveCD (b)\033[0m, \033[1;34minstalling from a LiveCD (i)\033[0m, or just \033[1;32mupdating your configuration (u)\033[0m? (default 'u' by [RET])" 
-    read -p "> (b/i/u) ... " flag
+    echo -e "\nWhich \033[1moperation\033[0m do you like to perform?
+        \033[31m- build a LiveCD            \033[0m(b)
+        \033[33m- install from a LiveCD     \033[0m(i)
+        \033[1;32m- update your configuration \033[0m(\033[1;4mu\033[0m)" 
+    read -p ">> " flag
 
-    if [ "$flag" != "b" ] && [ "$flag" != "B" ]
+    if [[ "$flag" != "b" && "$flag" != "B" ]]
     then
-	# if [ "$USER" != "root" ]
-	# then
-	#     exec echo "Please switch to root user."
-	# fi
-	echo -e "\nOverride rebuild command? (default 'switch' by [RET])\nFor some options, Check if you have root privilege."
-	read -p "> (switch/test/boot/build/dry-build/...) ... " override
+	echo -e "\nWhich rebuild \033[1;34msubcommand\033[0m do you like to use? (\033[1;4mswitch\033[0m/boot/test/build/dry-build/...)"
+	read -p ">> " operation
 
-	echo -e "\nPlease input your \033[1mhostname\033[0m. (default '$(hostname)' by [RET])"
-	read -p "> (hostname) ... " hname
+	echo -e "\nWhat will be your \033[1;35mideal hostname\033[0m? (\033[1;4m$(hostname)\033[0m/...)"
+	read -p ">> " hname
 	echo
     fi
 fi
 
 # ~~ Auto Completion ~~
-if [ "$flag" == "" ] ; then flag="u"; fi
-if [ "$override" == "" ] ; then override="switch"; fi
-if [ "$hname" == "" ] ; then hname=$(hostname); fi
+if [[ -z "$flag" ]] ; then flag="u"; fi
+if [[ -z "$operation" ]] ; then operation="switch"; fi
+if [[ -z "$hname" ]] ; then hname=$(hostname); fi
 
 
 # ~~ Action ~~
 # 1. update flake.lock
-if [ "$updateFlake" = "y" ]
+if [[ "$updateFlake" == "y" ]]
 then 
     nix flake update
 fi
 
 # 2. LiveCD
-if [ "$flag" = "b" ] || [ "$flag" = "B" ]
+if [[ "$flag" == "b" || "$flag" == "B" ]]
 then
     exec nix build path:.#nixosConfigurations.livecd.config.system.build.isoImage
 fi
 
 # 3. normal build
-if [ "$flag" = "i" ] || [ "$flag" = "I" ]
+if [[ "$flag" == "i" || "$flag" == "I" ]]
 then
     # TODO: Use `nixos-generate-config` or some other things to automatically update hardware-configuration.nix 
     # nixos-generate-config --root /mnt
     # cp /mnt/etc/hardware-configuration.nix ...towhere?
-    exec nixos-install --flake "path:.#$hname"
-elif [ "$flag" = "u" ] || [ "$flag" = "U" ]
+    echo -e "Oops! It requires you to \033[1;31mcheck your privilege\033[0m..."
+    exec sudo nixos-install --flake "path:.#$hname"
+elif [[ "$flag" == "u" || "$flag" == "U" ]]
 then
-    exec nixos-rebuild $override --flake "path:.#$hname" 
+    if [[ "$operation" != *"build" ]]
+    then
+	echo -e "Oops! It requires you to \033[1;31mcheck your privilege\033[0m..."
+	exec sudo nixos-rebuild $operation --flake "path:.#$hname"
+    else
+	exec nixos-rebuild $operation --flake "path:.#$hname"
+    fi
 fi
 
 # ~~ Error ~~
-echo -e "\033[1;31mInvalid input!\033[0m Exiting the build script..."
+echo -e "\n\033[1;31mInvalid input!\033[0m Exiting the build script..."
 exit 1
