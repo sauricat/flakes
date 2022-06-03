8f9fae1bb6d055866bffebb799bdad53f1015faf
sauricat <linshu1729@protonmail.com>
2022-06-03 23:58:59 +0800

    rime: make scanning config directory automatically and recursively

--- home/rime.nix
+++ home/rime.nix
@@ -1,20 +1,22 @@
 { config, lib, ... }:
+let
+  copyDir = fromDir: toDir: # fromDir is a path, toDir is a string.
+    lib.mapAttrs'
+      (name: value: lib.nameValuePair (toDir + "/" + name) (fromDir + "/${name}"))
+      (lib.filterAttrs (name: value: value == "regular")
+        (builtins.readDir fromDir));
+  copyDirRecursively = fromDir: toDir:
+    builtins.foldl' (a: b: a // b)
+      (copyDir fromDir toDir)
+      (lib.mapAttrsToList
+        (name: value: copyDirRecursively (fromDir + "/${name}") (toDir + "/" + name))
+        (lib.filterAttrs (name: value: value == "directory")
+          (builtins.readDir fromDir)));
+  mkHomeFile = fromDir: toDir:
+    lib.mapAttrs'
+      (name: value: lib.nameValuePair name ({ source = value; }))
+      (copyDirRecursively fromDir toDir);
+in
 {
-  home.file = lib.attrsets.mapAttrs' (name: value: 
-      lib.attrsets.nameValuePair 
-        (".config/ibus/rime/${value}") 
-        ({ source = config.lib.file.mkOutOfStoreSymlink ./rime/${value}; })){ 
-             dc = "default.custom.yaml";
-             kb2 = "key_bindings2.yaml";
-             sym = "mysymbols.yaml";
-             dpys = "double_pinyin_mspy.schema.yaml";
-             cs = "chaizi.schema.yaml"; 
-             cd = "chaizi.dict.yaml"; 
-             ws = "wugniu.schema.yaml";
-             wls = "wugniu_lopha.schema.yaml";
-             wld = "wugniu_lopha.dict.yaml";
-             ls = "langjin.schema.yaml";
-             ld = "langjin.dict.yaml";
-           };
-  
+  home.file = mkHomeFile ./rime ".config/ibus/rime";
 }
