6a926209e9d4eccad6dd307c2067be1b348e2909
sauricat <linshu1729@protonmail.com>
2022-06-07 22:51:11 +0800

    user: add user "oxa" <3

--- user.nix
+++ user.nix
@@ -2,11 +2,21 @@
 
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.shu = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    shell = pkgs.fish;
+  users.users = {
+    shu = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+      shell = pkgs.fish;
+    };
+    oxa = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      shell = pkgs.zsh;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYl9bIMoMrs8gWUmIAF42mGnKVxqY6c+g2gmE6u2E/B oxa@invar"
+      ];
+    };
   };
-
+  
   security.pam.services.shu.enableKwallet = true;
 }
