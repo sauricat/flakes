4311fac28bcdc7a7b1ff0dddb891f8a5cd41d46d
sauricat <linshu1729@protonmail.com>
2022-06-17 02:40:39 +0800

    comp: modify killwine service
    Under construction.

--- home/compatibility.nix
+++ home/compatibility.nix
@@ -16,12 +16,19 @@
     wemeet
   ];
   
-  systemd.user.services.killWineWeChat = {
+  systemd.user.services.killWine = {
     Unit = {
-      Description = "Kill Wine WeChat before shutting down, or it would get stuck.";
-      PartOf = "graphical-session.target";
+      Description = "Kill Wine and WeChat before shutting down, or it would get stuck.";
+      Before = [ "shutdown.target" ];
     };
-    Service.ExecStop = "${pkgs.procps}/bin/pkill -9 WeChat";
-    Install.WantedBy = [ "graphical-session.target" ];
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.writeScript "killwine.sh" ''
+          #!${pkgs.runtimeShell}
+          ${pkgs.procps}/bin/pkill -9 WeChat
+          ${pkgs.procps}/bin/ps -ef | ${pkgs.ripgrep}/bin/rg "C:\\\\" | ${pkgs.ripgrep}/bin/rg -v ${pkgs.ripgrep} | ${pkgs.gawk}/bin/gawk '{ print $2 }' | ${pkgs.findutils}/bin/xargs ${pkgs.util-linux}/bin/kill -9
+        ''}";
+    };
+    Install.WantedBy = [ "shutdown.target" ];
   };
 }
