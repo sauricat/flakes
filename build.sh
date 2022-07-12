096ab82a48c0971b119b364614240d73b7d900e1
sauricat <linshu1729@protonmail.com>
2022-07-12 21:32:10 +0800

    build.sh: improvements on interaction and function

--- build.sh
+++ build.sh
@@ -9,30 +9,45 @@ Usage:
 
 Options:
   -h, --help: Open help page.
-  -d, --default: Use default settings. 
+  -d, --default: Use default settings.
+  -c, --custom: After that, type the value of each question, separate them with <SPC>, and press <RET>.
 "
 fi
 
 # ~~ Interaction ~~
 if [[ "$1" != "-d" && "$1" != "--default" ]]
 then
-    echo -e "\nUpdate '\033[1;36mflake\033[0m.lock'? (y/\033[1;4mn\033[0m)"
-    read -p ">> " updateFlake
-
-    echo -e "\nWhich \033[1moperation\033[0m do you like to perform?
+    if [[ "$1" == "-c" || "$1" == "--custom" ]]
+    then
+	flag="$2"
+	subcommand="$3"
+	hname="$4"
+	other=""
+	while [[ $# -ne 4 ]]
+	do
+	    other+=" $5"
+	    shift
+	done
+    else
+	echo -e "\nWhich \033[1moperation\033[0m do you like to perform?
         \033[31m- build a LiveCD            \033[0m(b)
         \033[33m- install from a LiveCD     \033[0m(i)
-        \033[1;32m- update your configuration \033[0m(\033[1;4mu\033[0m)" 
-    read -p ">> " flag
+        \033[1;32m- update your configuration \033[0m(\033[1;4mu\033[0m)"
+	read -p ">> " flag
 
-    if [[ "$flag" != "b" && "$flag" != "B" ]]
-    then
-	echo -e "\nWhich rebuild \033[1;34msubcommand\033[0m do you like to use? (\033[1;4mswitch\033[0m/boot/test/build/dry-build/...)"
-	read -p ">> " subcommand
+	if [[ "$flag" != "b" && "$flag" != "B" ]]
+	then
+	    echo -e "\nWhich rebuild \033[1;34msubcommand\033[0m do you like to use? (\033[1;4mswitch\033[0m/boot/test/build/dry-build/...)"
+	    read -p ">> " subcommand
 
-	echo -e "\nWhat will be your \033[1;35mideal hostname\033[0m? (\033[1;4m$(hostname)\033[0m/...)"
-	read -p ">> " hname
-	echo
+	    echo -e "\nWhat will be your \033[1;35mideal hostname\033[0m? (\033[1;4m$(hostname)\033[0m/...)"
+	    read -p ">> " hname
+
+	    echo -e "\nIf you have other option, such as '--no-build-nix', '--show-trace', '--verbose', please indicate here; or simply press <RET>."
+	    read -p ">> " other
+
+	    echo
+	fi
     fi
 fi
 
@@ -41,36 +56,36 @@ if [[ -z "$flag" ]] ; then flag="u"; fi
 if [[ -z "$subcommand" ]] ; then subcommand="switch"; fi
 if [[ -z "$hname" ]] ; then hname=$(hostname); fi
 
-
-# ~~ Action ~~
-# 1. update flake.lock
-if [[ "$updateFlake" == "y" ]]
-then 
-    nix flake update
+if [[ -z "$1" ]]
+then
+    echo -e "\033[1;31mHint:\033[0m Next time you can type '\033[1;31m$0 -c $flag $subcommand $hname$other\033[0m' to execute the same operation.\n"
 fi
 
-# 2. LiveCD
+
+# ~~ Action ~~
+# 1. LiveCD
 if [[ "$flag" == "b" || "$flag" == "B" ]]
 then
     exec nix build path:.#nixosConfigurations.livecd.config.system.build.isoImage
 fi
 
-# 3. normal build
+
+# 2. normal build
 if [[ "$flag" == "i" || "$flag" == "I" ]]
 then
-    # TODO: Use `nixos-generate-config` or some other things to automatically update hardware-configuration.nix 
+    # TODO: Use `nixos-generate-config` or some other things to automatically update hardware-configuration.nix
     # nixos-generate-config --root /mnt
     # cp /mnt/etc/hardware-configuration.nix ...towhere?
     echo -e "Oops! It requires you to \033[1;31mcheck your privilege\033[0m..."
-    exec sudo nixos-install --flake "path:.#$hname"
+    exec sudo nixos-install --flake "path:.#$hname" $other
 elif [[ "$flag" == "u" || "$flag" == "U" ]]
 then
     if [[ "$subcommand" != *"build" ]]
     then
 	echo -e "Oops! It requires you to \033[1;31mcheck your privilege\033[0m..."
-	exec sudo nixos-rebuild $subcommand --flake "path:.#$hname"
+	exec sudo nixos-rebuild $subcommand --flake "path:.#$hname" $other
     else
-	exec nixos-rebuild $subcommand --flake "path:.#$hname"
+	exec nixos-rebuild $subcommand --flake "path:.#$hname" $other
     fi
 fi
 
