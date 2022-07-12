aae9167094e2e7371a747727daace9201b289a48
sauricat <linshu1729@protonmail.com>
2022-07-12 20:27:05 +0800

    console-l10n: delete cjktty patch

--- services/console-l10n.nix
+++ services/console-l10n.nix
@@ -1,17 +1,5 @@
 { pkgs, ... }:
-let
-  cjktty-5_15 = {
-    name = "cjktty-5.15";
-    patch = pkgs.fetchpatch {
-      name = "cjktty-5.15.patch";
-      url = "https://raw.githubusercontent.com/zhmars/cjktty-patches/2a35e3af97f1de47870096263c8eb101aa58004b/v5.x/cjktty-5.15.patch";
-      sha256 = "sha256-jYWYhR/WVBcG8AIKkNIoPc5DzgJSN4f4LfCOut+eyVQ=";
-    };
-  };
-in
 {
-  # boot.kernelPackages = pkgs.linuxKernel.packages.linux_5_15; # fix kernel version
-  # boot.kernelPatches = [ cjktty-5_15 ];
   services.kmscon = {
     enable = true;
     extraConfig = "font-size=20";
