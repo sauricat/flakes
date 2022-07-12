8f10750b3689fdd39e6bbe92b66f18f13b7c6c3f
sauricat <linshu1729@protonmail.com>
2022-07-12 20:18:32 +0800

    emacs/term: improve tmux session management

--- home/emacs/shu/shu-term.el
+++ home/emacs/shu/shu-term.el
@@ -8,7 +8,7 @@
   (vterm-mode . (vterm--toggle-mouse t))
   :bind ("C-c t t" . vterm)
   :config
-  (setq vterm-shell "tmux")
+  (setq vterm-shell "tmux new-session -t main")
   (define-key vterm-mode-map (kbd "<wheel-up>") [mouse-4])
   (define-key vterm-mode-map (kbd "<wheel-down>") [mouse-5]))
 
