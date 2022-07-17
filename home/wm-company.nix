11b56ec5fac68b02037bc3f19d74c0427792a2b5
sauricat <linshu1729@protonmail.com>
2022-07-17 16:36:29 +0800

    polybar: subtle change

--- home/wm-company.nix
+++ home/wm-company.nix
@@ -45,40 +45,15 @@ in
   systemd.user.services.polybar = lib.mkOverride 10 { }; # let exwm start it.
   services.polybar =
     let
-      colors = {
+      colors = rec {
         background = "#d6d4d4";
         background-alt = "#e4e4e4";
         foreground = "#000000";
         primary = "#4271ae";
-        secondary = "#c82829";
+        secondary = primary;
         alert = "#c82829";
         disabled = "#8a8787";
       };
-      find-ibus-engine = pkgs.writeShellScript "find-ibus-engine.sh" ''
-        IBUS_RAW=$(${pkgs.ibus}/bin/ibus engine)
-        IBUS_ENGINE=$(echo $IBUS_RAW | ${pkgs.gawk}/bin/awk -F ':' '{print $1}')
-        IBUS_LAYOUT=$(echo $IBUS_RAW | ${pkgs.gawk}/bin/awk -F ':' '{print $2}')
-        IBUS_LAYOUT_VARIANT=$(echo $IBUS_RAW | ${pkgs.gawk}/bin/awk -F ':' '{print $3}')
-
-        ENGINE_ICON="🚎"
-        LAYOUT_ICON=""
-
-        if [ "$IBUS_ENGINE" = "xkb" ]
-        then
-            IBUS_ENGINE=""
-        fi
-        OUTPUT="$ENGINE_ICON $IBUS_ENGINE"
-
-        if [ -n "$IBUS_LAYOUT_VARIANT" ] && [ "$IBUS_LAYOUT_VARIANT" != "\n" ]
-        then
-            OUTPUT="$OUTPUT $LAYOUT_ICON $IBUS_LAYOUT - $IBUS_LAYOUT_VARIANT"
-        else
-            OUTPUT="$OUTPUT $LAYOUT_ICON $IBUS_LAYOUT"
-        fi
-
-        echo $OUTPUT
-        exit 0
-      '';
     in
     {
       enable = true; # how to let exwm enable it?
@@ -113,7 +88,7 @@ in
 
         modules-left = xworkspaces
         modules-center = date
-        modules-right = memory cpu wlan eth pulseaudio ibus battery
+        modules-right = memory cpu wlan eth pulseaudio battery
 
         cursor-click = pointer
         cursor-scroll = ns-resize
@@ -131,19 +106,19 @@ in
         [module/xworkspaces]
         type = internal/xworkspaces
 
-        label-active = EXWM Workspace %name%: with %nwin% desktop app(s)
+        label-active = %{F${colors.secondary}}#%name%+%nwin%%{F-}
         label-active-background = ${colors.background-alt}
-        label-active-underline= ${colors.primary}
+        label-active-underline = ${colors.primary}
         label-active-padding = 1
 
-        label-occupied = #%name%+%nwin%
+        label-occupied = %{F${colors.primary}}#%name%%{F-}+%nwin%
         label-occupied-padding = 1
 
-        label-urgent = #%name%+%nwin%
+        label-urgent = %{F${colors.alert}}#%name%+%nwin%%{F-}
         label-urgent-background = ${colors.alert}
         label-urgent-padding = 1
 
-        label-empty = #%name%+%nwin%
+        label-empty = %{F${colors.primary}}#%name%%{F${colors.disabled}}+%nwin%%{F-}
         label-empty-foreground = ${colors.disabled}
         label-empty-padding = 1
 
@@ -189,17 +164,18 @@ in
         interval = 5
         format-connected = <label-connected>
         format-disconnected = <label-disconnected>
-        label-disconnected = %{F${colors.primary}}%ifname%%{F-} disconnected
 
         [module/wlan]
         inherit = network-base
         interface-type = wireless
-        label-connected = %{F${colors.primary}}%ifname%%{F-} %essid% %local_ip%
+        label-connected = %{F${colors.primary}}WLAN%{F-} %essid%
+        label-disconnected = %{F${colors.disabled}}WLAN%{F-}
 
         [module/eth]
         inherit = network-base
         interface-type = wired
-        label-connected = %{F${colors.primary}}%ifname%%{F-} %local_ip%
+        label-connected = %{F${colors.primary}}ETH%{F-} connected
+        label-disconnected = %{F${colors.disabled}}ETH%{F-}
 
         [module/battery]
         type = internal/battery
@@ -229,15 +205,6 @@ in
         [settings]
         screenchange-reload = true
         pseudo-transparency = true
-
-        [module/ibus]
-        type = custom/script
-
-        exec = ${find-ibus-engine} &
-        ; you need to run ibus-daemon first for the script to work
-        ; exec-if = ${pkgs.procps}/bin/pgrep -x ibus-daemon
-
-        tail = true
       '';
     };
 }
