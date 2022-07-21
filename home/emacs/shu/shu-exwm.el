5b28f4553f375175e3e2b7105cb09b11e250a611
sauricat <linshu1729@protonmail.com>
2022-07-21 19:04:07 +0800

    exwm: refactor exwm keys

--- home/emacs/shu/shu-exwm.el
+++ home/emacs/shu/shu-exwm.el
@@ -2,6 +2,86 @@
 ;;; Commentary:
 "Configuration of exwm."
 ;;; Code:
+
+(defun shu-exwm-start-process-gui-command (cmd)
+  "A temporary solution when `start-process-shell-command' doesn't work for X apps (marked as CMD)."
+  (progn (multi-vterm-dedicated-open) ;; temporary solution
+         (message cmd)
+         (vterm-send-string (format "%s\n" cmd))
+         (multi-vterm-dedicated-close)))
+;; (start-process-shell-command cmd nil cmd))
+(defun shu-exwm-lock-screen ()
+  "Lock screen."
+  (interactive)
+  (shu-exwm-start-process-gui-command "i3lock-shu"))
+(defun shu-exwm-screenshot ()
+  "Take a screenshot."
+  (interactive)
+  (shu-exwm-start-process-gui-command "set a ~/Pictures/Screenshots/$(date +%s).png; maim $a; xclip -selection clipboard $a -t image/png"))
+(defun shu-exwm-screenshot-area ()
+  "Take a screenshot of a mouse selected area."
+  (interactive)
+  (shu-exwm-start-process-gui-command "set a ~/Pictures/Screenshots/$(date +%s).png; maim -s $a; xclip -selection clipboard $a -t image/png"))
+(defun shu-exwm-d-launcher ()
+  "Open rofi desktop app launcher."
+  (interactive)
+  (shu-exwm-start-process-gui-command "rofi -show drun"))
+
+(defvar shu-exwm-keymap
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "s-r") 'exwm-reset) ;; reset to line mode
+    (define-key map (kbd "s-d") 'exwm-workspace-delete)
+    (define-key map (kbd "s-a") 'exwm-workspace-add)
+    (define-key map (kbd "s-w") 'exwm-workspace-swap)
+    (define-key map (kbd "s-k") 'kill-current-buffer)
+    (define-key map (kbd "s-<return>") 'multi-vterm)
+    (define-key map (kbd "s-s") 'shu-exwm-d-launcher)
+    (define-key map (kbd "<print>") 'shu-exwm-screenshot-area)
+    (define-key map (kbd "s-<print>") 'shu-exwm-screenshot)
+    (define-key map (kbd "s-l") 'shu-exwm-lock-screen)
+    (define-key map (kbd "s-<up>") 'windmove-up)
+    (define-key map (kbd "s-<down>") 'windmove-down)
+    (define-key map (kbd "s-<right>") 'windmove-right)
+    (define-key map (kbd "s-<left>") 'windmove-left)
+    (define-key map (kbd "s-S-<up>") 'windmove-swap-states-up)
+    (define-key map (kbd "s-S-<down>") 'windmove-swap-states-down)
+    (define-key map (kbd "s-S-<right>") 'windmove-swap-states-right)
+    (define-key map (kbd "s-S-<left>") 'windmove-swap-states-left)
+    (define-key map (kbd "s-p") 'windmove-up)
+    (define-key map (kbd "s-n") 'windmove-down)
+    (define-key map (kbd "s-f") 'windmove-right)
+    (define-key map (kbd "s-b") 'windmove-left)
+    (define-key map (kbd "s-P") 'windmove-swap-states-up)
+    (define-key map (kbd "s-N") 'windmove-swap-states-down)
+    (define-key map (kbd "s-F") 'windmove-swap-states-right)
+    (define-key map (kbd "s-B") 'windmove-swap-states-left)
+    (define-key map (kbd "s-g") 'exwm-floating-toggle-floating)
+    (define-key map (kbd "s-+") ;; We always need to stay in line mode.
+                `(lambda ()
+                   (interactive)
+                   (let ((id (exwm--buffer->id (current-buffer))))
+                     (exwm-layout-toggle-fullscreen id)
+                     (exwm-input-grab-keyboard id))))
+    (dotimes (i 11)
+      (define-key map (kbd (format "s-%d" (mod i 10)))
+                          `(lambda ()
+                             (interactive)
+                             (exwm-workspace-switch-create ,(- i 1))))
+      (define-key map (kbd (concat "s-"
+                                       (pcase (mod i 10)
+                                         (1 "!") (2 "@") (3 "#") (4 "$") (5 "%")
+                                         (6 "^") (7 "&") (8 "*") (9 "(") (0 ")"))))
+                          `(lambda ()
+                             (interactive)
+                             (exwm-workspace-move-window ,(- i 1))
+                             (exwm-workspace-switch ,(- i 1)))))
+    (define-key map (kbd "s-S")
+                        (lambda (command)
+                          (interactive (list (read-shell-command "$ ")))
+                          (start-process-shell-command command nil command)))
+    map)
+  "Keymap for shu's exwm shortkeys.")
+
 (use-package exwm
   :config
   (defun exwm-rename-buffer () ;; show app name in every exwm buffer
@@ -17,73 +97,13 @@
 
   (setq exwm-workspace-number 4)
 
-  (defun start-process-gui-command (cmd)
-    "A temporary solution when `start-process-shell-command' doesn't work for X apps."
-    (progn (multi-vterm-dedicated-open) ;; temporary solution
-           (message cmd)
-           (vterm-send-string (format "%s\n" cmd))
-           (multi-vterm-dedicated-close)))
-    ;; (start-process-shell-command cmd nil cmd))
-  (defun wm-lock-screen ()
-    (interactive)
-    (start-process-gui-command "i3lock-shu"))
-  (defun wm-screenshot ()
-    (interactive)
-    (start-process-gui-command "set a ~/Pictures/Screenshots/$(date +%s).png; maim $a; xclip -selection clipboard $a -t image/png"))
-  (defun wm-screenshot-area ()
-    (interactive)
-    (start-process-gui-command "set a ~/Pictures/Screenshots/$(date +%s).png; maim -s $a; xclip -selection clipboard $a -t image/png"))
-  (defun wm-d-launcher ()
-    (interactive)
-    (start-process-gui-command "rofi -show drun"))
+  (mapcar (lambda (item)
+          (pcase item
+            (`(,key . ,func) (exwm-input-set-key `[,key] func))))
+        (remove 'keymap shu-exwm-keymap))
+  (mapcar (lambda (item) (push item exwm-input-prefix-keys))
+          (mapcar 'car (remove 'keymap shu-exwm-keymap)))
 
-  (exwm-input-set-key (kbd "s-r") 'exwm-reset) ;; reset to line mode
-  (exwm-input-set-key (kbd "s-d") 'exwm-workspace-delete)
-  (exwm-input-set-key (kbd "s-a") 'exwm-workspace-add)
-  (exwm-input-set-key (kbd "s-w") 'exwm-workspace-swap)
-  (exwm-input-set-key (kbd "s-k") 'kill-this-buffer)
-  (exwm-input-set-key (kbd "s-<return>") 'multi-vterm)
-  (exwm-input-set-key (kbd "s-s") 'wm-d-launcher)
-  (exwm-input-set-key (kbd "<print>") 'wm-screenshot-area)
-  (exwm-input-set-key (kbd "s-<print>") 'wm-screenshot)
-  (exwm-input-set-key (kbd "s-l") 'wm-lock-screen)
-  (exwm-input-set-key (kbd "s-<up>") 'windmove-up)
-  (exwm-input-set-key (kbd "s-<down>") 'windmove-down)
-  (exwm-input-set-key (kbd "s-<right>") 'windmove-right)
-  (exwm-input-set-key (kbd "s-<left>") 'windmove-left)
-  (exwm-input-set-key (kbd "s-S-<up>") 'windmove-swap-states-up)
-  (exwm-input-set-key (kbd "s-S-<down>") 'windmove-swap-states-down)
-  (exwm-input-set-key (kbd "s-S-<right>") 'windmove-swap-states-right)
-  (exwm-input-set-key (kbd "s-S-<left>") 'windmove-swap-states-left)
-  (exwm-input-set-key (kbd "s-p") 'windmove-up)
-  (exwm-input-set-key (kbd "s-n") 'windmove-down)
-  (exwm-input-set-key (kbd "s-f") 'windmove-right)
-  (exwm-input-set-key (kbd "s-b") 'windmove-left)
-  (exwm-input-set-key (kbd "s-P") 'windmove-swap-states-up)
-  (exwm-input-set-key (kbd "s-N") 'windmove-swap-states-down)
-  (exwm-input-set-key (kbd "s-F") 'windmove-swap-states-right)
-  (exwm-input-set-key (kbd "s-B") 'windmove-swap-states-left)
-  (dotimes (i 11)
-    (exwm-input-set-key (kbd (format "s-%d" (mod i 10)))
-                        `(lambda ()
-                           (interactive)
-                           (exwm-workspace-switch-create ,(- i 1))))
-    (exwm-input-set-key (kbd (concat "s-"
-                                     (pcase (mod i 10)
-                                       (1 "!") (2 "@") (3 "#") (4 "$") (5 "%")
-                                       (6 "^") (7 "&") (8 "*") (9 "(") (0 ")"))))
-                        `(lambda ()
-                           (interactive)
-                           (exwm-workspace-move-window ,(- i 1))
-                           (exwm-workspace-switch ,(- i 1)))))
-
-  ;; the simplest launcher, I keep it in only if dmenu eventually stopped working or something
-  (exwm-input-set-key (kbd "s-S") ;; caps:none
-                      (lambda (command)
-                        (interactive (list (read-shell-command "$ ")))
-                        (start-process-shell-command command nil command)))
-
-  ;; an easy way to make keybindings work *only* in line mode
   (push ?\C-q exwm-input-prefix-keys)
   (define-key exwm-mode-map [?\C-q] #'exwm-input-send-next-key)
 
