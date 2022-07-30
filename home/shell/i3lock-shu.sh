18b71c9eaba4afee8c087bf3369dfab4a5af249c
sauricat <linshu1729@protonmail.com>
2022-07-31 06:06:35 +0800

    i3lock: add a cooldown time

--- home/shell/i3lock-shu.sh
+++ home/shell/i3lock-shu.sh
@@ -4,20 +4,45 @@
 
 pic=~/clash-configuration/lock-screen-picture
 picresized=$(mktemp)
+picresizedhint=$(mktemp)
 
 screenarea=$(xrandr --current | grep -oP '(?<=current )\d+ x \d+')
 screenarea=${screenarea// /}
 screens=( $(xrandr --current | grep -oP '\d+x\d+\+\d+\+\d+') )
 
 convert -size $screenarea xc:black -quality 11 png24:"$picresized"
+convert -size $screenarea xc:black -quality 11 png24:"$picresizedhint"
 
-for screen in "${screens[@]}"; do
+for screen in "${screens[@]}"
+do
     convert "$picresized" \
             \( "$pic" -gravity Center -resize ${screen%%+*}^ -extent ${screen%%+*} \) \
             -gravity NorthWest -geometry +${screen#*+} -composite \
             -quality 11 png24:"$picresized"
+    convert "$picresizedhint" \
+            \( "$pic" -gravity Center -resize ${screen%%+*}^ -extent ${screen%%+*} \
+            -font DejaVu-Sans -fill white -stroke black -pointsize 96 -strokewidth 2 \
+            -draw "text 5,5 'Locking... Press [q]/[x] to relieve.'" \) \
+            -gravity NorthWest -geometry +${screen#*+} -composite \
+            -quality 11 png24:"$picresizedhint"
 done
-pkill onboard || true
+
+feh --fullscreen --auto-zoom "$picresizedhint" &
+pidfeh=$!
+sleep 2
+if [[ -z $(ps -ef | awk '{print $2}' | grep $pidfeh) ]]
+then
+    exit
+else
+    kill $pidfeh
+fi
+if [[ -z $(pgrep onboard) ]]
+then
+    $existsonboard=
+else
+    pkill onboard
+    $existsonboard=0
+fi
 i3lock-color -i "$picresized"\
              --pass-{media,volume,power,screen}-keys\
              --line-color=ffffff00 --separator-color=ffffff22\
@@ -28,8 +53,13 @@ i3lock-color -i "$picresized"\
              --{verif,wrong,lock,lockfailed,noinput,greeter}-text= --no-modkey-text\
              --ind-pos="x+w-228:y+h-32-140"  --time-pos="x+w-24:y+h-24-140" --time-align=2\
              --{verif,wrong,greeter}-size=20 --time-size=24 &
-(onboard || true) &
-while [[ -n $(pgrep i3lock-color || true) ]]; do
+onboard &
+pidonboardnew=$!
+while [[ -n $(pgrep i3lock-color) ]]
+do
     sleep 0.5
 done
-pkill onboard || true
+if [[ -z $existsonboard ]]
+then
+    kill $pidonboardnew
+fi
