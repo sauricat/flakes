0c1fde564db17ee84ff5d31e387ed69481544563
sauricat <linshu1729@protonmail.com>
2022-07-31 06:08:13 +0800

    bluetooth: add blueman

--- local-modules/bluetooth.nix
+++ local-modules/bluetooth.nix
@@ -3,10 +3,10 @@
 {
   # Bluetooth
   hardware.bluetooth.enable = true;
-  # services.blueman.enable = true;
-  services.dbus.packages = [ 
-    pkgs.bluez 
-    # pkgs.blueman 
+  services.blueman.enable = true;
+  services.dbus.packages = [
+    pkgs.bluez
+    pkgs.blueman
   ];
   systemd.tmpfiles.rules = [ # See https://github.com/NixOS/nixpkgs/issues/170573
     "d /var/lib/bluetooth 700 root root - -"
