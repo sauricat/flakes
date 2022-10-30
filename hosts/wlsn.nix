dbc605cfb73b9d9e0094a89145b71d6b9fe88398
sauricat <linshu1729@protonmail.com>
2022-10-30 14:46:31 -0400

    wlsn: add ntfs

--- hosts/wlsn.nix
+++ hosts/wlsn.nix
@@ -4,7 +4,7 @@
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
-
+  boot.supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelModules = [ "kvm-intel" "hid-apple" ];
