fd0c170171e29eaa061fc2861dbff9dd69f06d65
sauricat <saurica@kazv.moe>
2026-02-20 16:31:05 -0500

    emacs: remove deprecated epkg

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -219,25 +219,6 @@
   :mode (rx ".gleam" eos))
 
 ;; Dired
-(use-package dired-single
-  :config
-  (defun my-dired-init ()
-    "Bunch of stuff to run for dired, either immediately or when it's
-   loaded."
-    ;; <add other stuff here>
-    (define-key dired-mode-map [remap dired-find-file]
-                'dired-single-buffer)
-    (define-key dired-mode-map [remap dired-mouse-find-file-other-window]
-                'dired-single-buffer-mouse)
-    (define-key dired-mode-map [remap dired-up-directory]
-                'dired-single-up-directory))
-
-  ;; if dired's already loaded, then the keymap will be bound
-  (if (boundp 'dired-mode-map)
-      ;; we're good to go; just add our bindings
-      (my-dired-init)
-    ;; it's not loaded yet, so add our bindings to the load-hook
-    (with-eval-after-load 'dired-mode (my-dired-init))))
 (use-package dirvish
   :config (dirvish-override-dired-mode))
 
