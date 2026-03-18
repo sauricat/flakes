a163b8727453727bff1b0682eed174908dd640ea
sauricat <linshu1729@protonmail.com>
2026-03-17 17:57:45 -0700

    Revert "iwkr: Switch time zone"
    This reverts commit 7aa1bfd23db54d774f7357a37dd7cb262e8a070e.
    
    back to TO

--- hosts/iwkr.nix
+++ hosts/iwkr.nix
@@ -80,7 +80,7 @@
 
   networking.networkmanager.wifi.powersave = true;
 
-  time.timeZone = "America/Vancouver";
+  time.timeZone = "America/Toronto";
   # Don't change this version.
   system.stateVersion = "21.11";
 
